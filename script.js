330925f9ac7b34c3c6adf25579e2720b17b55cf0
wolfgray <5565486@mail.ru>
2023-01-17 04:55:53 +0300

    add tab fn

--- script.js
+++ script.js
@@ -2,13 +2,34 @@ const modalW = document.querySelector('.modal-menu')
 const burgerOpen = document.querySelector('.burger-menu')
 const burgerClose = document.querySelector('.modal-burger-menu')
 const header = document.querySelector('.header')
+const item = document.querySelectorAll('.item')
+const activeTab = document.querySelectorAll('.tab')
 
 
-console.log(burgerOpen);
-console.log(burgerClose);
+  for (let el of activeTab) {
+    el.addEventListener('click', () => {
+      for (let el of activeTab) {
+        el.classList.remove('active') 
+      }
+      el.classList.add('active')
+      for (let i=0; i<activeTab.length; i++) {
+      item[i].classList.remove('item-active')
+
+        if (activeTab[i].classList.contains('active')) {
+          item[i].classList.add('item-active')
+        }
+       }
+    })
+  }
+  for (let i=0; i<activeTab.length; i++) {
+    if (activeTab[i].classList.contains('active')) {
+      item[i].classList.add('item-active')
+    }
+   }
+  
+
 
 burgerOpen.addEventListener("click", () => {
-  console.log('click burger');
   modalW.classList.toggle('hide')
   modalW.classList.toggle('open')
 
